8a9600ebd277e939278b77b6739c222d9ac2f461
David Havlicek <snex00@protonmail.com>
2024-02-17 09:52:03 -0800

    enable quiet mode with custom exit codes

--- validate.rb
+++ validate.rb
@@ -9,6 +9,16 @@ PLAYER_LISTS = 'https://xmrvsbeast.com/p2pool/select_lists'.freeze
 
 TIMESTAMP_VALID_WITHIN = (60 * 60).freeze # 1 hour
 
+ERR_NO_RECENT_WINNERS = 1.freeze
+ERR_NO_WINNER_AT_HEIGHT = 2.freeze
+ERR_NO_BLOCKHASH = 3.freeze
+ERR_BAD_TIMESTAMP = 4.freeze
+ERR_BLOCKHASH_NO_MATCH_HEIGHT = 5.freeze
+ERR_NO_ROUND_TYPES = 6.freeze
+ERR_BAD_ROUND_TYPE = 7.freeze
+ERR_NO_PLAYERS_LIST = 8.freeze
+ERR_BAD_WINNER = 9.freeze
+
 require 'csv'
 require 'json'
 require 'open-uri'
@@ -27,6 +37,10 @@ class Parser
 
       opts.on('--explorer EXPLORER', String, 'XMR Exolorer URL - REQUIRED. Must be an instance of the Onion Monero Blockchain explorer, such as https://xmrchain.net/.') do |x|
         op[:explorer] = x
+      end
+
+      opts.on('-q', '--quiet', 'Do not print output.') do
+        op[:quiet] = true
       end
 
       opts.on('-h', '--help', 'Prints this help') do
@@ -48,14 +62,14 @@ if !options.has_key?(:explorer)
   exit 1
 end
 
-print 'Validating XMR height exists in winners list... '
+print 'Validating XMR height exists in winners list... ' unless options[:quiet]
 
 begin
   recent_winners = CSV.parse(URI.open(RECENT_WINNERS).read, col_sep: "\t")
 rescue => e
-  puts FAIL
-  puts "Unable to download #{RECENT_WINNERS}, please check your internet connection."
-  exit 1
+  puts FAIL unless options[:quiet]
+  puts "Unable to download #{RECENT_WINNERS}, please check your internet connection." unless options[:quiet]
+  exit ERR_NO_RECENT_WINNERS
 end
 
 winner = if options[:height]
@@ -66,87 +80,87 @@ winner = if options[:height]
          end
 
 if !winner
-  puts FAIL
-  puts "No winner found matching height #{options[:height]}. Please check that you entered it correctly and have internet access."
-  exit 1
+  puts FAIL unless options[:quiet]
+  puts "No winner found matching height #{options[:height]}. Please check that you entered it correctly and have internet access." unless options[:quiet]
+  exit ERR_NO_WINNER_AT_HEIGHT
 end
 
-puts PASS
+puts PASS unless options[:quiet]
 
-print 'Validating height matches timestamp... '
+print 'Validating height matches timestamp... ' unless options[:quiet]
 reported_ts = Time.parse(winner[1])
 
 begin
   xmr_block = JSON.parse(URI.open("#{options[:explorer]}/api/block/#{options[:height]}").read)
 rescue
-  puts FAIL
-  puts "Unable to fetch blockchain hash from #{options[:explorer]}/api/block/#{options[:height]}, please check your internet connection."
-  exit 1
+  puts FAIL unless options[:quiet]
+  puts "Unable to fetch blockchain hash from #{options[:explorer]}/api/block/#{options[:height]}, please check your internet connection." unless options[:quiet]
+  exit ERR_NO_BLOCKHASH
 end
 
 xmr_ts = Time.parse(xmr_block['data']['timestamp_utc'])
 timediff = (xmr_ts - reported_ts).abs.to_i
 
 if timediff <= TIMESTAMP_VALID_WITHIN
-  puts PASS
+  puts PASS unless options[:quiet]
 else
-  puts FAIL
-  puts "Reported timestamp is #{timediff} seconds away from XMR height, larger than the maximum of #{TIMESTAMP_VALID_WITHIN}."
-  exit 1
+  puts FAIL unless options[:quiet]
+  puts "Reported timestamp is #{timediff} seconds away from XMR height, larger than the maximum of #{TIMESTAMP_VALID_WITHIN}." unless options[:quiet]
+  exit ERR_BAD_TIMESTAMP
 end
 
-print 'Validating height matches block hash... '
+print 'Validating height matches block hash... ' unless options[:quiet]
 reported_hash = winner[5]
 xmr_hash = xmr_block['data']['hash'][0..11]
 
 if reported_hash == xmr_hash
-  puts PASS
+  puts PASS unless options[:quiet]
 else
-  puts FAIL
-  puts "Reported XMR blockhash (#{reported_hash}) is different from actual blockhash at height #{options[:height]} (#{xmr_hash})."
-  exit 1
+  puts FAIL unless options[:quiet]
+  puts "Reported XMR blockhash (#{reported_hash}) is different from actual blockhash at height #{options[:height]} (#{xmr_hash})." unless options[:quiet]
+  exit ERR_BLOCKHASH_NO_MATCH_HEIGHT
 end
 
-print 'Validating round_type... '
+print 'Validating round_type... ' unless options[:quiet]
 rolls = winner[6].split('/').last
 reported_round = winner[8]
 
 begin
   round_types = URI.open(ROUND_TYPES).read.split("\n")
 rescue
-  puts FAIL
-  puts "Unable to fetch round_types data from #{ROUND_TYPES}, please check your internet connection."
-  exit 1
+  puts FAIL unless options[:quiet]
+  puts "Unable to fetch round_types data from #{ROUND_TYPES}, please check your internet connection." unless options[:quiet]
+  exit ERR_NO_ROUND_TYPES
 end
 
 expected_round = Roller.roll(xmr_hash, round_types, rolls)
 
 if reported_round == expected_round
-  puts PASS
+  puts PASS unless options[:quiet]
 else
-  puts FAIL
-  puts "Reported round_type (#{reported_round}) is different from expected round (#{expected_round})."
-  exit 1
+  puts FAIL unless options[:quiet]
+  puts "Reported round_type (#{reported_round}) is different from expected round (#{expected_round})." unless options[:quiet]
+  exit ERR_BAD_ROUND_TYPE
 end
 
-print 'Validating winner... '
+print 'Validating winner... ' unless options[:quiet]
 rolls = winner[6].split('/').first
 reported_winner = winner[0][0..7]
 
 begin
   player_list = URI.open("#{PLAYER_LISTS}/#{xmr_hash}-players.txt").read.split("\n")
 rescue
-  puts FAIL
-  puts "Unable to fetch players data from #{PLAYER_LISTS}/#{xmr_hash}-players.txt, please check your internet connection."
-  exit 1
+  puts FAIL unless options[:quiet]
+  puts "Unable to fetch players data from #{PLAYER_LISTS}/#{xmr_hash}-players.txt, please check your internet connection." unless options[:quiet]
+  exit ERR_NO_PLAYERS_LIST
 end
 
 expected_winner = Roller.roll(xmr_hash, player_list, rolls)
 
 if reported_winner == expected_winner
-  puts PASS
+  puts PASS unless options[:quiet]
 else
-  puts FAIL
-  puts "Reported winner (#{reported_winner}) is different from expected winner (#{expected_winner})."
-  exit 1
+  puts FAIL unless options[:quiet]
+  puts "Reported winner (#{reported_winner}) is different from expected winner (#{expected_winner})." unless options[:quiet]
+  exit ERR_BAD_WINNER
 end
